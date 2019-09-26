33f9b4a2991f87bbf0ad4e5c6705b1e974c8249b
twelvefifteen <54555927+twelvefifteen@users.noreply.github.com>
2019-09-26 15:55:58 -0500

    Removed unused function, added MIT license

--- tf_image_statistics.h
+++ tf_image_statistics.h
@@ -341,18 +341,6 @@ TFIS_sRGBToLinearV4(tfis_v4 A)
 }
 
 static tfis_v4
-TFIS_LinearTosRGBV4(tfis_v4 A)
-{
-    tfis_v4 Result;
-    Result.x = TFIS_LinearTosRGB(A.x);
-    Result.y = TFIS_LinearTosRGB(A.y);
-    Result.z = TFIS_LinearTosRGB(A.z);
-    Result.w = TFIS_Clamp01(A.w);
-    
-    return(Result);
-}
-
-static tfis_v4
 TFIS_UnpackRGBA(tfis_u32 C)
 {
     tfis_f32 Inv255 = (1.0f / 255.0f);
@@ -860,3 +848,27 @@ TFISFree(void* Address)
 }
 
 #endif
+
+/*
+MIT License
+
+Copyright (c) 2019 Gustavo Velasquez (@twelvefifteen on GitHub)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this softwareand associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright noticeand this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
